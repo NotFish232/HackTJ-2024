d65d4c928b4cf986df77fd8b24e2212d93808a77
Justin Lee <2025jlee@tjhsst.edu>
2024-02-24 17:38:45 -0500

    add stream traffic video

--- website/apis/stream_traffic_video.py
+++ website/apis/stream_traffic_video.py
@@ -1,19 +1,59 @@
-import requests 
+import requests
 import shutil
+from typing import Generator
+import os
+import cv2
 
-base_url =  "https://itsvideo.arlingtonva.us:8013/live/cam214.stream"
-url = f"{base_url}/playlist.m3u8"
+FPS = 15
+IMG_SIZE = 512
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+with open(f"{current_dir}/available_camera_feeds.txt", "r") as f:
+    AVAILABLE_CAMERA_FEEDS = f.read().split("\n")
 
 
+def get_live_feed(base_url: str) -> str:
+    initial_url = f"{base_url}/playlist.m3u8"
 
-x = requests.get(url).text.split("\n")[-2]
-print(x)
-y = []
-for line in requests.get(f"{base_url}/{x}").text.split("\n"):
-    if line.endswith("ts"):
-        y.append(line)
-print(requests.get(f"{base_url}/{x}").text)
-for j in y:
-    with requests.get(f"{base_url}/{j}", stream=True) as r:
-        with open(j, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
+    r = requests.get(initial_url)
+    chunk_url = next(l for l in r.text.split("\n") if l.endswith(".m3u8"))
+
+    r = requests.get(f"{base_url}/{chunk_url}")
+    ts_urls = [l for l in r.text.split("\n") if l.endswith(".ts")]
+
+    out_url = f"{current_dir}/temp/{base_url.rsplit('/', -1)[-1].split('.', 1)[0]}.mp4"
+    writer = cv2.VideoWriter(
+        out_url, cv2.VideoWriter_fourcc(*"mp4v"), FPS, (IMG_SIZE, IMG_SIZE)
+    )
+
+    for ts_url in ts_urls:
+        with requests.get(f"{base_url}/{ts_url}", stream=True) as r:
+            with open(f"{current_dir}/temp/{ts_url}", "wb") as f:
+                shutil.copyfileobj(r.raw, f)
+
+        path = f"{current_dir}/temp/{ts_url}"
+        cap = cv2.VideoCapture(path)
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
+            writer.write(frame)
+
+        cap.release()
+
+        os.remove(path)
+
+    writer.release()
+
+    return out_url
+
+
+def main() -> None:
+    get_live_feed(AVAILABLE_CAMERA_FEEDS[1])
+
+
+if __name__ == "__main__":
+    main()
