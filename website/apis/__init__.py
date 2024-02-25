219e9139beef71cdf4557771e124a2329bf463b5
Justin Lee <2025jlee@tjhsst.edu>
2024-02-24 21:15:36 -0500

    rename live traffic feed

--- website/apis/__init__.py
+++ website/apis/__init__.py
@@ -1,4 +1,4 @@
 from .get_person_description import get_person_description
 from .get_vehicle_description import get_vehicle_description
-from .stream_traffic_video import AVAILABLE_CAMERA_FEEDS, get_live_feed
+from .get_live_feed import AVAILABLE_CAMERA_FEEDS, get_live_feed
 from .get_license_plate import get_license_plate
