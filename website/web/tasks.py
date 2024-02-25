6c4ac7ccdd815483281efa8a7f321644a27f605c
Justin Lee <2025jlee@tjhsst.edu>
2024-02-25 05:04:35 -0500

    move to relative paths

--- website/web/tasks.py
+++ website/web/tasks.py
@@ -26,6 +26,10 @@ app = Celery("tasks", broker="redis://localhost//")
 image_dir = Path(__file__).parent.parent / "apis/dataset"
 
 
+def absolute_to_relative(path: str) -> str:
+    return f"./{path[path.index('website'):]}"
+
+
 @app.task
 def run_facial_detection(
     initial_image: str = str(image_dir / "initial_image.png"),
@@ -34,25 +38,33 @@ def run_facial_detection(
         file = str(file)
         detection_result = get_facial_detection(file, initial_image)
         FacialDetectionResult(
-            source_image=file,
-            target_image=initial_image,
+            source_image=absolute_to_relative(file),
+            target_image=absolute_to_relative(initial_image),
             found_match=detection_result is not None,
-            box_result=detection_result[0] if detection_result else None,
-            cropped_result=detection_result[1] if detection_result else None,
+            box_result=(
+                absolute_to_relative(detection_result[0]) if detection_result else None
+            ),
+            cropped_result=(
+                absolute_to_relative(detection_result[1]) if detection_result else None
+            ),
         ).save()
 
 
 @app.task
 def run_person_description(image: str = str(image_dir / "initial_image.png")) -> None:
     description = get_person_description(image)
+    image = absolute_to_relative(image)
     PersonDescriptionResult(image=image, description=description).save()
 
 
 @app.task
 def run_vehicles(image: str = str(image_dir / "vehicles.png")) -> None:
     box_image, sub_cars = get_vehicles(image)
+    image = absolute_to_relative(image)
+    box_image = absolute_to_relative(box_image)
 
     for cropped_image, desc in sub_cars:
+        cropped_image = absolute_to_relative(cropped_image)
         VehicleIdentificationResult(
             image=image,
             box_result=box_image,
@@ -64,7 +76,10 @@ def run_vehicles(image: str = str(image_dir / "vehicles.png")) -> None:
 @app.task
 def get_all_people(image: str = str(image_dir / "facial-detection-2.png")) -> None:
     box_image, sub_image_paths = get_people(image)
+    image = absolute_to_relative(image)
+    box_image = absolute_to_relative(box_image)
     for cropped_image in sub_image_paths:
+        cropped_image = absolute_to_relative(cropped_image)
         PeopleGetAllResult(
             image=image, box_result=box_image, cropped_result=cropped_image
         ).save()
@@ -73,4 +88,12 @@ def get_all_people(image: str = str(image_dir / "facial-detection-2.png")) -> No
 @app.task
 def get_license_plate_str(image: str = str(image_dir / "license_plate.png")) -> None:
     license_plate = get_license_plate(image)
+    image = absolute_to_relative(image)
     LicensePlateResult(image=image, license_plate=license_plate).save()
+
+
+# run_facial_detection()
+# run_person_description()
+# run_vehicles()
+# get_all_people()
+# get_license_plate_str()
